81ae825a233acfa7f1fc2398c701f0261e9fb135
pennyfine5 <pennyfine5@users.noreply.github.com>
2026-01-05 14:09:55 -0600

    correccion de grafiacas pt.2

--- app.py
+++ app.py
@@ -10,17 +10,16 @@ df_cars = pd.read_csv('vehicles_us.csv')
 st.header('Análisis de Datos de Vehículos')
 
 
-print(df_cars.head())
+st.write(df_cars.head())
+
 
 
 fig = px.histogram(df_cars, x="odometer") # crear un histograma
-fig.show() # crear gráfico de dispersión
-
+st.plotly_chart(fig, use_container_width=True) # crear gráfico de dispersión
 
 
 fig = px.scatter(df_cars, x="odometer", y="price") # crear un gráfico de dispersión
-fig.show() # crear gráfico de dispersión
-
+st.plotly_chart(fig, use_container_width=True) # crear gráfico de dispersión
 
 
 # Mostrar información básica (opcional)
